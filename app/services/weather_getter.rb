2149dd361cb4bcb8cc34501c8e5201d797dc8d1e
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2022-03-10 17:23:08 -0500

    Creating rspecs.

--- app/services/weather_getter.rb
+++ app/services/weather_getter.rb
@@ -2,20 +2,24 @@ class WeatherGetter
   API_KEY = 'e155ba888319d33e0b85dec3fa93ef64'
   API_URL = "https://api.openweathermap.org/data/2.5/weather"
   DEFAULT_CACHE_EXPIRATION_TIME = 30.minutes
-  DEFAULT_OPTIONS = {appid: API_KEY, units: 'metric'}
+  DEFAULT_OPTIONS = {units: 'metric'}
 
-  def self.call(searchKey, searchValue, cache_expiration_time = DEFAULT_CACHE_EXPIRATION_TIME, options = DEFAULT_OPTIONS)
-    return OpenStruct.new(success?: true) if searchValue.blank?
+  def self.call(searchKey, searchValue, options = DEFAULT_OPTIONS)
+    return OpenStruct.new(success?: false) if searchValue.blank? || searchKey.blank?
     options[searchKey] = searchValue
     OpenStruct.new(success?: true,
                    cache_data?: exist_in_cache?(options),
-                   result: get_cached_result(cache_expiration_time, options))
+                   result: get_cached_result(options))
   rescue => e
     OpenStruct.new(success?: false, error: e.message)
   end
 
-  def self.get_by_zipcode(zipcode)
-    self.call(:zip, zipcode)
+  def self.get_by_zipcode(zipcode, options = DEFAULT_OPTIONS)
+    self.call(:zip, zipcode, options)
+  end
+
+  def self.get_by_city(city, options = DEFAULT_OPTIONS)
+    self.call(:q, city, options)
   end
 
   private
@@ -24,11 +28,11 @@ class WeatherGetter
     Rails.cache.exist?(key)
   end
 
-  def self.get_cached_result(cache_expiration_time, options)
-    Rails.cache.fetch(options, expires_in: cache_expiration_time) { self.get_json(options) }
+  def self.get_cached_result(options)
+    Rails.cache.fetch(options, expires_in: DEFAULT_CACHE_EXPIRATION_TIME) { self.get_json(options) }
   end
 
   def self.get_json(options)
-    JSON.parse RestClient.get API_URL, {params: {**options}}
+    JSON.parse RestClient.get API_URL, {params: {appid: API_KEY, **options}}
   end
 end
---
+++ spec/services/weather_getter_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe WeatherGetter do
+
+  let(:response) { 'good json response with weather data' }
+  let(:http_error_response) { 'http error' }
+  let(:options) { {test: 'paramValue'} }
+
+  subject { described_class }
+
+  describe '#call' do
+    context 'good weather response not cached' do
+      before do
+        allow(subject).to receive(:get_cached_result).and_return response
+        allow(subject).to receive(:exist_in_cache?).and_return false
+      end
+
+      it 'returns the weather data' do
+        data = subject.call(:searchKey, "searchValue", options)
+        expect(data.success?).to be true
+        expect(data.result).to be response
+        expect(data.cache_data?).to be false
+      end
+    end
+
+    context 'good weather response cached' do
+      before do
+        allow(subject).to receive(:get_cached_result).and_return response
+        allow(subject).to receive(:exist_in_cache?).and_return true
+      end
+
+      it 'returns the weather data' do
+        data = subject.call(:searchKey, "searchValue", options)
+        expect(data.success?).to be true
+        expect(data.result).to be response
+        expect(data.cache_data?).to be true
+        expect(data.error).to be nil
+      end
+    end
+
+    context 'no search params' do
+      it 'should return just a success?=true without trying to pull data' do
+        data = subject.call(:zip, "", options)
+        expect(data.success?).to be false
+        expect(data.result).to be nil
+      end
+    end
+
+    context 'http error response' do
+      before do
+        allow(subject).to receive(:get_cached_result).and_raise http_error_response
+      end
+
+      it 'returns the weather data' do
+        data = subject.call(:searchKey, "searchValue", options)
+        expect(data.success?).to be false
+        expect(data.result).to be nil
+        expect(data.error).to be http_error_response
+      end
+    end
+  end
+end
